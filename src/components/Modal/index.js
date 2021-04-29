019bb6aeedeefe7d372a2938b89b178372c1211a
goniti <gonzalezlovullocarlos@hotmail.fr>
2021-04-29 20:55:59 +0200

    adding icon on button

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -42,7 +42,7 @@ const Modal = ({
 						<div className="suggest__validate">
 							<span>{`${handleLimit[1]}`}</span>
 							<div className="button--bordered">
-								Valider mes choix <GoTasklist size={16} />
+								Valider mes choix <span className="suggest__validate--icon"><GoTasklist size={16} /></span> 
 							</div>
 						</div>
 					) : (
