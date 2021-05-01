e07dcaf25629ec380c3d86903cb45d5a7bec00bc
goniti <gonzalezlovullocarlos@hotmail.fr>
2021-05-01 10:08:14 +0200

    review header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,39 +1,36 @@
 import './header.scss'
+import PropTypes from 'prop-types'
 import { GoPlusSmall } from 'react-icons/go'
 
 const Header = ({ handleTitleModal, touristPlaces, showModal }) => {
+	console.log(touristPlaces)
 	const noPlaces = Object.entries(touristPlaces).length === 0
-	let canStillSave = false
-	let remainingZone = 0
-
-	if (!noPlaces) {
-		const countZone = Object.entries(touristPlaces).length
-		const limitZone = 3
-		canStillSave = touristPlaces.data.length < limitZone
-		remainingZone = limitZone - countZone
-	}
 
 	return (
 		<header className="App__header" onClick={() => handleTitleModal("Création d'une zone")}>
-			{noPlaces && (
-				<>
-					<p className="App__header__text">Aucune zone enregistrée</p>
-					<button onClick={showModal} className="button--bordered">
-						Créez en une
-					</button>
-				</>
-			)}
-			{canStillSave && (
-				<>
-					<p>{`Vous pouvez encore enregistrer ${remainingZone} zone(s)`}</p>
-					<button className="button--bordered" onClick={showModal}>
-						Ajouter
-						<GoPlusSmall size={16} />
-					</button>
-				</>
-			)}
+			{noPlaces && <p className="App__header__text">Aucune zone enregistrée</p>}
+			<button onClick={showModal} className="button--bordered">
+				Ajouter une zone <GoPlusSmall size={16} />
+			</button>
 		</header>
 	)
+}
+
+Header.propTypes = {
+	touristPlaces: PropTypes.shape({
+		data: PropTypes.arrayOf(
+			PropTypes.shape({
+				municipality: PropTypes.string,
+				pictures: PropTypes.arrayOf(
+					PropTypes.shape({
+						alt: PropTypes.string,
+						id: PropTypes.string,
+						src: PropTypes.string,
+					}).isRequired
+				),
+			})
+		),
+	}),
 }
 
 export default Header
